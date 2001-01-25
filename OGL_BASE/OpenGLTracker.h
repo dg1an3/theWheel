6af0cc1b42e9ed3d33c560f74a655bdf758daab7
dlane <>
2001-01-25 19:03:40 +0000

    OGL_BASE 	OpenGLView 	OpenGLRenderer 	OpenGLTracker 	OpenGLTexture

---
+++ OGL_BASE/OpenGLTracker.h
@@ -0,0 +1,31 @@
+// OpenGLTracker.h: interface for the COpenGLTracker class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_OPENGLTRACKER_H__1369AB31_F2ED_11D4_9E3C_00B0D0609AB0__INCLUDED_)
+#define AFX_OPENGLTRACKER_H__1369AB31_F2ED_11D4_9E3C_00B0D0609AB0__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+class COpenGLView;
+
+class COpenGLTracker  
+{
+public:
+	COpenGLTracker(COpenGLView *pView);
+	virtual ~COpenGLTracker();
+
+	virtual BOOL IsInside(CPoint point) { return TRUE; }
+
+	virtual void OnLButtonDown(UINT nFlags, CPoint point) { }
+	virtual void OnLButtonUp(UINT nFlags, CPoint point)   { }
+	virtual void OnMouseMove(UINT nFlags, CPoint point)   { }
+	virtual void OnMouseDrag(UINT nFlags, CPoint point)   { }
+
+protected:
+	COpenGLView *m_pView;
+};
+
+#endif // !defined(AFX_OPENGLTRACKER_H__1369AB31_F2ED_11D4_9E3C_00B0D0609AB0__INCLUDED_)
