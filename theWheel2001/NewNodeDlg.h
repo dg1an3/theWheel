927c0aba4228e76f03c203a564cf0e44f0fabcc8
dlane <>
2001-02-10 01:50:00 +0000

    Initial import of theWheel concept mapper

---
+++ theWheel2001/NewNodeDlg.h
@@ -0,0 +1,47 @@
+#if !defined(AFX_NEWNODEDLG_H__8E566675_F7C8_11D4_9E3E_00B0D0609AB0__INCLUDED_)
+#define AFX_NEWNODEDLG_H__8E566675_F7C8_11D4_9E3E_00B0D0609AB0__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+// NewNodeDlg.h : header file
+//
+
+/////////////////////////////////////////////////////////////////////////////
+// CNewNodeDlg dialog
+
+class CNewNodeDlg : public CDialog
+{
+// Construction
+public:
+	CNewNodeDlg(CWnd* pParent = NULL);   // standard constructor
+
+// Dialog Data
+	//{{AFX_DATA(CNewNodeDlg)
+	enum { IDD = IDD_NEWNODE };
+	CString	m_strName;
+	CString	m_strDesc;
+	//}}AFX_DATA
+
+
+// Overrides
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CNewNodeDlg)
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+	//}}AFX_VIRTUAL
+
+// Implementation
+protected:
+
+	// Generated message map functions
+	//{{AFX_MSG(CNewNodeDlg)
+		// NOTE: the ClassWizard will add member functions here
+	//}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+};
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_NEWNODEDLG_H__8E566675_F7C8_11D4_9E3E_00B0D0609AB0__INCLUDED_)
