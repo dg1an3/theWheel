0945a7d1c721c9105589dba34e4a0f085a329386
dlane <>
2001-02-12 22:27:12 +0000

    Importing MODEL_BASE library: 	CObserver/CObservable 	CValue with SyncTo mechanism 	CAssociation 	CAutoSyncValue 	CCollection 	CMatrix 	CVector and more!

---
+++ MODEL_BASE/AutoSyncValue.h
@@ -0,0 +1,58 @@
+// CAutoSyncValue.h: interface for the CSyncConfigurator class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_SYNCCONFIGURATOR_H__CF990024_FD24_11D4_9E44_00B0D0609AB0__INCLUDED_)
+#define AFX_SYNCCONFIGURATOR_H__CF990024_FD24_11D4_9E44_00B0D0609AB0__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#include "Observer.h"
+#include "Association.h"
+
+template<class TYPE, class MEMBER_TYPE>
+class CAutoSyncValue : public CValue<MEMBER_TYPE>
+{
+public:
+	CAutoSyncValue(CAssociation<TYPE> *pAssoc, 
+		CValue<MEMBER_TYPE> TYPE::*pMember)
+		: m_pAssoc(pAssoc),
+			m_pMember(pMember)
+	{
+		m_pAssoc->AddObserver(this);
+
+		// trigger an update
+		OnChange(m_pAssoc);
+	}
+
+	virtual ~CAutoSyncValue()
+	{
+		m_pAssoc->RemoveObserver(this);
+	}
+
+	virtual void OnChange(CObservable *pSource)
+	{
+		if (pSource == m_pAssoc)
+		{
+			TYPE *pObj = (TYPE *) m_pAssoc->Get();
+			
+			// if we are pointing to something
+			if (pObj != NULL)
+				// sync the member
+				SyncTo(&(pObj->*m_pMember));
+			else
+				// sync to nothing
+				SyncTo(NULL);
+		}
+		else
+			CValue<MEMBER_TYPE>::OnChange(pSource);
+	}
+
+private:
+	CAssociation<TYPE> *m_pAssoc;
+	CValue<MEMBER_TYPE> TYPE::*m_pMember;
+};
+
+#endif // !defined(AFX_SYNCCONFIGURATOR_H__CF990024_FD24_11D4_9E44_00B0D0609AB0__INCLUDED_)
