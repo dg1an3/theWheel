48a22c905c1f2671721287f2eac75250e4488013
dlane <>
2001-02-14 01:11:52 +0000

    Importing MODEL_BASE library: 	CObserver/CObservable 	CValue with SyncTo mechanism 	CAssociation 	CAutoSyncValue 	CCollection 	CMatrix 	CVector and more!

---
+++ MODEL_BASE/StdAfx.h
@@ -0,0 +1,40 @@
+// stdafx.h : include file for standard system include files,
+//  or project specific include files that are used frequently, but
+//      are changed infrequently
+//
+
+#if !defined(AFX_STDAFX_H__4CA62886_01F1_11D5_9E46_00B0D0609AB0__INCLUDED_)
+#define AFX_STDAFX_H__4CA62886_01F1_11D5_9E46_00B0D0609AB0__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#define VC_EXTRALEAN		// Exclude rarely-used stuff from Windows headers
+
+#include <afx.h>
+#ifdef _WINDOWS
+#include <afxwin.h>
+#endif
+
+#include <afxtempl.h>
+
+#define CREATE_TAB(LENGTH) \
+	CString TAB; \
+	while (TAB.GetLength() < LENGTH * 2) \
+		TAB += "\t\t"
+
+#define DC_TAB(DUMP_CONTEXT) \
+	DUMP_CONTEXT << TAB
+
+#define PUSH_DUMP_DEPTH(DUMP_CONTEXT) \
+	int OLD_DUMP_DEPTH = DUMP_CONTEXT.GetDepth(); \
+	DUMP_CONTEXT.SetDepth(OLD_DUMP_DEPTH + 1)
+
+#define POP_DUMP_DEPTH(DUMP_CONTEXT) \
+	DUMP_CONTEXT.SetDepth(OLD_DUMP_DEPTH)
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_STDAFX_H__4CA62886_01F1_11D5_9E46_00B0D0609AB0__INCLUDED_)
