86f4e1b1fe2bccd5858797a16225bf819dfe86cb
dlane <>
2001-02-20 21:27:45 +0000

    Adding the GUI_BASE module, containing 	ObjectExplorer 	ObjectExplorerBar 	TablControlBar

---
+++ GUI_BASE/GUI_BASE_resource.h
@@ -0,0 +1,15 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by GUI_BASE.rc
+//
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        603
+#define _APS_NEXT_COMMAND_VALUE         40601
+#define _APS_NEXT_CONTROL_VALUE         1600
+#define _APS_NEXT_SYMED_VALUE           601
+#endif
+#endif
