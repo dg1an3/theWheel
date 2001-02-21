9d8fddbf033bdd7f1d46a514e74a86202cc600ea
dlane <>
2001-02-21 18:48:03 +0000

    Optimizer base -- contains object-oriented code for implementing various 	optimizations 	Brent (1-D) 	Powell 	Conjugate Gradient

---
+++ OPTIMIZER_BASE/Optimizer.h
@@ -0,0 +1,50 @@
+// Optimizer.h: interface for the COptimizer class.
+//
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(OPTIMIZER_H)
+#define OPTIMIZER_H
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#include <Vector.h>
+#include <Value.h>
+#include "ObjectiveFunction.h"
+
+template<int DIM, class TYPE>
+class COptimizer
+{
+public:
+	COptimizer(CObjectiveFunction<DIM, TYPE> *pFunc)
+		: m_pFunc(pFunc),
+			tolerance((TYPE) 0.1),
+			iteration(0)
+	{
+	}
+
+	virtual ~COptimizer()
+	{
+	}
+
+	// defines the tolerance for exit from optimization loop
+	CValue< TYPE > tolerance;
+
+	// holds the number of iterations needed for the optimization
+	CValue< int > iteration;
+
+	// holds the final value of the optimization
+	CValue< TYPE > finalValue;
+
+	// holds the final value of the parameters for the minimum f
+	CValue< CVector<DIM, TYPE> > finalParam;
+
+	// function to actually perform the optimization
+	virtual CVector<DIM, TYPE> Optimize(const CVector<DIM, TYPE>& vInit) = 0;
+
+protected:
+	CObjectiveFunction<DIM, TYPE> *m_pFunc;
+};
+
+#endif // !defined(OPTIMIZER_H)
---
+++ OPTIMIZER_BASE/StdAfx.h
@@ -0,0 +1,25 @@
+// stdafx.h : include file for standard system include files,
+//  or project specific include files that are used frequently, but
+//      are changed infrequently
+//
+
+#if !defined(AFX_STDAFX_H__70763E23_0829_11D5_9E4C_00B0D0609AB0__INCLUDED_)
+#define AFX_STDAFX_H__70763E23_0829_11D5_9E4C_00B0D0609AB0__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#define VC_EXTRALEAN		// Exclude rarely-used stuff from Windows headers
+
+#include <afx.h>
+#ifdef _WINDOWS
+#include <afxwin.h>
+#endif
+
+#include <afxtempl.h>
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_STDAFX_H__70763E23_0829_11D5_9E4C_00B0D0609AB0__INCLUDED_)
