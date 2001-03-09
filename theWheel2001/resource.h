a350ce24341a44d8a6195e4f373636d1a9427005
dlane <>
2001-03-09 02:11:04 +0000

    Adding node icon

--- theWheel2001/resource.h
+++ theWheel2001/resource.h
@@ -3,15 +3,14 @@
 // Used by theWheel2001.rc
 //
 #define IDD_ABOUTBOX                    100
-#define ID_VIEW_ARRANGE                 127
 #define IDR_MAINFRAME                   128
 #define IDR_WLSPCTYPE                   129
 #define IDC_HANDPOINT                   130
 #define IDR_SPACE_POPUP                 130
 #define IDD_NEWNODE                     131
+#define IDB_NODE                        609
 #define IDC_EDITNAME                    1000
 #define IDC_EDITDESC                    1001
-#define ID_TEST_ANIM                    32771
 #define ID_NEW_NODE                     32773
 #define ID_VIEW_LAYOUT                  32774
 #define ID_VIEW_PROPAGATE               32775
@@ -22,7 +21,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_3D_CONTROLS                     1
-#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_RESOURCE_VALUE        133
 #define _APS_NEXT_COMMAND_VALUE         32777
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
