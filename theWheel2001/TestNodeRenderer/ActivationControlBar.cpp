f09fda5dd68c02b27c234ac8c6e422de3a152a87
DLane <>
2001-08-07 01:55:51 +0000

    Added test program for the CNodeRenderer

---
+++ theWheel2001/TestNodeRenderer/ActivationControlBar.cpp
@@ -0,0 +1,54 @@
+// ActivationControlBar.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "TestNodeRenderer.h"
+#include "ActivationControlBar.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+/////////////////////////////////////////////////////////////////////////////
+// CActivationControlBar dialog
+
+
+CActivationControlBar::CActivationControlBar(CWnd* pParent /*=NULL*/)
+//	: CDialog(CActivationControlBar::IDD, pParent)
+{
+	//{{AFX_DATA_INIT(CActivationControlBar)
+		// NOTE: the ClassWizard will add member initialization here
+	//}}AFX_DATA_INIT
+}
+
+
+void CActivationControlBar::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogBar::DoDataExchange(pDX);
+	//{{AFX_DATA_MAP(CActivationControlBar)
+		// NOTE: the ClassWizard will add DDX and DDV calls here
+	//}}AFX_DATA_MAP
+}
+
+
+BEGIN_MESSAGE_MAP(CActivationControlBar, CDialogBar)
+	//{{AFX_MSG_MAP(CActivationControlBar)
+	ON_WM_VSCROLL()
+	//}}AFX_MSG_MAP
+END_MESSAGE_MAP()
+
+/////////////////////////////////////////////////////////////////////////////
+// CActivationControlBar message handlers
+
+void CActivationControlBar::OnVScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar) 
+{
+	if (nSBCode == TB_THUMBTRACK)
+	{
+		TRACE1("Slider position = %i\n", nPos);
+		activation.Set((double) nPos / 100.0);
+	}
+		
+	CDialogBar::OnVScroll(nSBCode, nPos, pScrollBar);
+}
