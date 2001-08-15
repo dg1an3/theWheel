507b195e9279f0da0e22fae90fea67ae7b8795aa
DLane <>
2001-08-15 00:17:21 +0000

    enhanced light object

--- OGL_BASE/OpenGLLight.h
+++ OGL_BASE/OpenGLLight.h
@@ -23,6 +23,12 @@ public:
 public:
 	// stores the position of the light
 	CValue< CVector<3> > position;
+
+	// diffuse color for the light
+	CValue< COLORREF > diffuseColor;
+
+	// call to set up the light in the current OpenGL rendering context
+	void TurnOn(int nLightNum = 0);
 };
 
 #endif // !defined(AFX_OPENGLLIGHT_H__61F32F74_8AC7_11D5_ABAE_00B0D0AB90D6__INCLUDED_)
