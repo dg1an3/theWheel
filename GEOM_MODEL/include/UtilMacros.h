fcd7bcd072ee5378c4437e44b58c559202a3a629
default <>
2002-06-03 05:35:01 +0000

    Moved utility macros (TRACE, etc.) to a separate include file

---
+++ GEOM_MODEL/include/UtilMacros.h
@@ -0,0 +1,53 @@
+//////////////////////////////////////////////////////////////////////
+// UtilMacros.h: standard utility macros
+//
+// Copyright (C) 1999-2001
+// $Id$
+//////////////////////////////////////////////////////////////////////
+
+#if !defined(UTILMACROS_H)
+#define UTILMACROS_H
+
+
+//////////////////////////////////////////////////////////////////////
+// Macros for trace dumps
+//////////////////////////////////////////////////////////////////////
+
+#define CREATE_TAB(LENGTH) \
+	CString TAB; \
+	while (TAB.GetLength() < LENGTH * 2) \
+		TAB += "\t\t"
+
+#define DC_TAB(DUMP_CONTEXT) \
+	DUMP_CONTEXT << TAB
+
+#define PUSH_DUMP_DEPTH(DUMP_CONTEXT) \
+	int OLD_DUMP_DEPTH = DUMP_CONTEXT.GetDepth(); \
+	DUMP_CONTEXT.SetDepth(OLD_DUMP_DEPTH + 1)
+
+#define POP_DUMP_DEPTH(DUMP_CONTEXT) \
+	DUMP_CONTEXT.SetDepth(OLD_DUMP_DEPTH)
+
+
+//////////////////////////////////////////////////////////////////////
+// Macros for serialization
+//////////////////////////////////////////////////////////////////////
+
+#define SERIALIZE_VALUE(ar, value) \
+	if (ar.IsLoading())				\
+	{								\
+		ar >> value;				\
+	}								\
+	else							\
+	{								\
+		ar << value;				\
+	}
+
+#define SERIALIZE_ARRAY(ar, array) \
+	if (ar.IsLoading())				\
+	{								\
+		array.RemoveAll();			\
+	}								\
+	array.Serialize(ar);
+
+#endif
