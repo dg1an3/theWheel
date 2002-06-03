b96cd7229921f6c59e4cd187cc5881a8e5fa0e74
default <>
2002-06-03 05:36:02 +0000

    Changed interface to use the CPackedVector array

--- GEOM_MODEL/include/Surface.h
+++ GEOM_MODEL/include/Surface.h
@@ -1,5 +1,8 @@
+//////////////////////////////////////////////////////////////////////
 // Surface.h: interface for the CSurface class.
 //
+// Copyright (C) 2000-2001
+// $Id$
 //////////////////////////////////////////////////////////////////////
 
 #if !defined(AFX_SURFACE_H__C7A6AA31_E5D9_11D4_9E2F_00B0D0609AB0__INCLUDED_)
@@ -9,30 +12,48 @@
 #pragma once
 #endif // _MSC_VER > 1000
 
-#include <ModelObject.h>
-#include <Value.h>
-#include <Association.h>
-
+// vectors
 #include <Vector.h>
-#include <Polygon.h>
 
-#include <Volumep.h>
+// modelobject base class
+#include "ModelObject.h"
 
+// polygons
+#include "Polygon.h"
+
+// volume (for region)
+#include "Volumep.h"
+
+
+//////////////////////////////////////////////////////////////////////
+// class CSurface
+//
+// represents a meshed surface
+//////////////////////////////////////////////////////////////////////
 class CSurface : public CModelObject
 {
 public:
+	// constructor
 	CSurface();
-	CSurface(const CSurface& fromSurface);
-	DECLARE_SERIAL(CSurface)
 
+	// copy constructor
+	CSurface(const CSurface& fromSurface);
+
+	// destructor
 	virtual ~CSurface();
 
+	// serialization support
+	DECLARE_SERIAL(CSurface)
+
+	// assignment
 	CSurface& operator=(const CSurface& fromSurface);
 
+	// contour accessors
 	int GetContourCount() const;
 	CPolygon& GetContour(int nIndex) const;
 	double GetContourRefDist(int nIndex) const;
 
+	// triangle accessors
 	int GetTriangleCount();
 	const double *GetTriangleVertex(int nTriangle, int nVertex);
 	const double *GetTriangleNormal(int nTriangle, int nVertex);
@@ -49,14 +70,14 @@ public:
 	//		MUST call CSurface::FireChange() as soon as possible to
 	//		notify observers of the change
 	CArray<int, int>& GetIndexArray() { return m_arrVertIndex; }
-	CArray<CVector<3>, CVector<3>&>& 
+	CArray<CPackedVector<3>, CPackedVector<3>&>& 
 		GetVertexArray() { return m_arrVertex; }
-	CArray<CVector<3>, CVector<3>&>& 
+	CArray<CPackedVector<3>, CPackedVector<3>&>& 
 		GetNormalArray() { return m_arrNormal; }
 
 	// accessor for the "region" -- volume with a 1.0 everywhere
 	//		that is in the structure
-	CAssociation< CVolume<int> > region;
+	CVolume<int> *m_pRegion;
 
 	// serialization
 	void Serialize(CArchive &ar);
@@ -66,26 +87,44 @@ public:
 	virtual void Dump(CDumpContext& dc) const;
 #endif
 
-public:
+private:
+	// contours for the structure
 	CObArray m_arrContours;
+
+	// reference distances for the contours
 	CArray<double, double> m_arrRefDist;
 
-private:
+	// mesh vertices
 	CArray<int, int> m_arrVertIndex;
-	CArray<CVector<3>, CVector<3>&> m_arrVertex;
-	CArray<CVector<3>, CVector<3>&> m_arrNormal;
+	CArray<CPackedVector<3>, CPackedVector<3>&> m_arrVertex;
+	CArray<CPackedVector<3>, CPackedVector<3>&> m_arrNormal;
 };
 
+//////////////////////////////////////////////////////////////////////
+// CSurface::GetTriangleCount
+// 
+// returns the number of triangles in the mesh
+//////////////////////////////////////////////////////////////////////
 inline int CSurface::GetTriangleCount()
 {
 	return m_arrVertIndex.GetSize() / 3;
 }
 
+//////////////////////////////////////////////////////////////////////
+// CSurface::GetTriangleVertex
+// 
+// returns the vertex for a specific triangle
+//////////////////////////////////////////////////////////////////////
 inline const double *CSurface::GetTriangleVertex(int nTriangle, int nVertex)
 {
 	return m_arrVertex[m_arrVertIndex[nTriangle*3 + nVertex]-1];
 }
 
+//////////////////////////////////////////////////////////////////////
+// CSurface::GetTriangleNormal
+// 
+// returns the normal for a specific triangle
+//////////////////////////////////////////////////////////////////////
 inline const double *CSurface::GetTriangleNormal(int nTriangle, int nVertex)
 {
 	return m_arrNormal[m_arrVertIndex[nTriangle*3 + nVertex]-1];
