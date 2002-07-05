549aa0f426d09be91092b4c7e2e66e0ca281ffde
default <>
2002-07-05 02:09:39 +0000

    Altered resources

--- theWheel2001/resource.h
+++ theWheel2001/resource.h
@@ -6,21 +6,19 @@
 #define IDR_MAINFRAME                   102
 #define IDR_WLSPCTYPE                   103
 #define IDB_DITHER50                    104
+#define IDC_SUPERNODECOUNT              1001
+#define IDC_TOLERANCE                   1002
+#define IDC_K_POS                       1003
+#define IDC_K_REP                       1004
 
 // Next default values for new objects
 // 
-// Ranges are:
-//					Application		Appl.View		GEOM_VIEW
-//		Resources	101   - 199		201   - 299		301   - 399
-//		Commands	40001 -	40999	39001 -	39999	38001 - 38999
-//		Controls	1001  -	1999	2001  - 2999	3001  - 3999
-//
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_3D_CONTROLS                     1
-#define _APS_NEXT_RESOURCE_VALUE        105
-#define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_RESOURCE_VALUE        106
+#define _APS_NEXT_COMMAND_VALUE         40002
+#define _APS_NEXT_CONTROL_VALUE         1005
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
