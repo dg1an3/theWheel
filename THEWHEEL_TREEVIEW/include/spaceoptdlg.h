f600f874852283e91cc7901bc8038010cb618de8
default <>
2003-05-18 18:00:50 +0000

    Separated from THEWHEEL_VIEW

---
+++ THEWHEEL_TREEVIEW/include/spaceoptdlg.h
@@ -0,0 +1,53 @@
+#if !defined(AFX_SPACEOPTDLG_H__271AFAE1_22CA_4C53_BC2A_E307DF555E95__INCLUDED_)
+#define AFX_SPACEOPTDLG_H__271AFAE1_22CA_4C53_BC2A_E307DF555E95__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+// SpaceOptDlg.h : header file
+//
+
+class CSpace;
+
+/////////////////////////////////////////////////////////////////////////////
+// CSpaceOptDlg dialog
+
+class CSpaceOptDlg : public CDialog
+{
+// Construction
+public:
+	CSpaceOptDlg(CWnd* pParent = NULL);   // standard constructor
+
+// Dialog Data
+	//{{AFX_DATA(CSpaceOptDlg)
+	enum { IDD = IDD_SPACEOPTPROP };
+	UINT	m_nSuperNodeCount;
+	double	m_tolerance;
+	double	m_kpos;
+	double	m_krep;
+	//}}AFX_DATA
+
+	// pointer to the space
+	CSpace * m_pSpace;
+
+// Overrides
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CSpaceOptDlg)
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+	//}}AFX_VIRTUAL
+
+// Implementation
+protected:
+
+	// Generated message map functions
+	//{{AFX_MSG(CSpaceOptDlg)
+	afx_msg void OnKillfocusAny();
+	//}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+};
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_SPACEOPTDLG_H__271AFAE1_22CA_4C53_BC2A_E307DF555E95__INCLUDED_)
---
+++ THEWHEEL_TREEVIEW/spacepropdlg.cpp
@@ -0,0 +1,73 @@
+// SpacePropDlg.cpp : implementation file
+//
+
+#include "stdafx.h"
+
+// resource includes
+#include "THEWHEEL_TREEVIEW_resource.h"
+
+#include <Space.h>
+
+#include "SpacePropDlg.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+/////////////////////////////////////////////////////////////////////////////
+// CSpacePropDlg dialog
+
+
+CSpacePropDlg::CSpacePropDlg(CWnd* pParent /*=NULL*/)
+	: CDialog(CSpacePropDlg::IDD, pParent),
+		m_pSpace(NULL)
+{
+	//{{AFX_DATA_INIT(CSpacePropDlg)
+	m_spring = 0.1;
+	m_primSec = 0.1;
+	//}}AFX_DATA_INIT
+}
+
+
+void CSpacePropDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+
+	if (!pDX->m_bSaveAndValidate && m_pSpace != NULL)
+	{
+		m_spring = m_pSpace->GetSpringConst();
+		m_primSec = m_pSpace->GetPrimSecRatio();
+	}
+
+	//{{AFX_DATA_MAP(CSpacePropDlg)
+	DDX_Text(pDX, IDC_SPRING, m_spring);
+	DDV_MinMaxDouble(pDX, m_spring, 0.1, 0.9);
+	DDX_Text(pDX, IDC_PRIMSEC, m_primSec);
+	DDV_MinMaxDouble(pDX, m_primSec, 0.1, 10.);
+	//}}AFX_DATA_MAP
+
+	if (pDX->m_bSaveAndValidate && m_pSpace != NULL)
+	{
+		m_pSpace->SetSpringConst(m_spring);
+		m_pSpace->SetPrimSecRatio(m_primSec);
+	}
+}
+
+
+BEGIN_MESSAGE_MAP(CSpacePropDlg, CDialog)
+	//{{AFX_MSG_MAP(CSpacePropDlg)
+	ON_EN_KILLFOCUS(IDC_PRIMSEC, OnKillfocusAny)
+	ON_EN_KILLFOCUS(IDC_SPRING, OnKillfocusAny)
+	//}}AFX_MSG_MAP
+END_MESSAGE_MAP()
+
+/////////////////////////////////////////////////////////////////////////////
+// CSpacePropDlg message handlers
+
+
+void CSpacePropDlg::OnKillfocusAny() 
+{
+	UpdateData();
+}
