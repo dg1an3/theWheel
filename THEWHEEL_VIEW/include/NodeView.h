ec72c86aecbbae6853d67275ac62350af445111f
default <>
2003-08-31 21:54:48 +0000

    Cleaned up header

--- THEWHEEL_VIEW/include/NodeView.h
+++ THEWHEEL_VIEW/include/NodeView.h
@@ -35,86 +35,89 @@ class CNodeView : public CObject
 {
 // Construction
 public:
-	CNodeView(CNode *pNode, CSpaceView *pParent);
-	virtual ~CNodeView();
+    CNodeView(CNode *pNode, CSpaceView *pParent);
+    virtual ~CNodeView();
 
 // Attributes
-public:
+    // accessor for the node
+    CNode *GetNode();
 
-	// accessor for the node
-	CNode *GetNode();
+    // moves the nodeview slowly toward its actual center
+    CVectorD<3> GetSpringCenter();
+    CVectorD<3> GetScaledNodeCenter();
 
-	// moves the nodeview slowly toward its actual center
-	CVectorD<3> GetSpringCenter();
-	
-	// activation value for this window
-	REAL GetThresholdedActivation();
-	REAL GetSpringActivation();
+    // activation value for this window
+    REAL GetThresholdedActivation();
+    REAL GetSpringActivation();
 
-	// accessors for the node view's rectangles
-	CRect& GetOuterRect();
-	CRect& GetInnerRect();
+    // accessors for the node view's rectangles
+    CRect& GetOuterRect();
+    CRect& GetInnerRect();
 
-	// accessor to shape descriptions of the node view
-	CRgn& GetShape();
+    // accessor to shape descriptions of the node view
+    CRgn& GetShape();
 
 // Operations
 public:
-	// smooth updating of spring state variables
-	void UpdateSprings(REAL springConst = 0.95);
+    // smooth updating of spring state variables
+	void UpdateSpringPosition(REAL springConst = 0.95);
+	void UpdateSpringActivation(REAL springConst = 0.95);
 
-	// draws the node view
-	void Draw(CDC *pDC);
+    // draws the node view
+    void Draw(CDC *pDC);
 
-	// draws the links for this nodeview
-	void DrawLinks(CDC *pDC, CNodeViewSkin *pSkin);
+    // draws the links for this nodeview
+    void DrawLinks(CDC *pDC, CNodeViewSkin *pSkin);
 
-	// adds an amount of pending activation
-	void AddPendingActivation(double pending);
+    // adds an amount of pending activation
+    void AddPendingActivation(double pending);
 
 // Implementation
 public:
-	void ResetPendingActivation();
-	REAL GetPendingActivation();
+	// helper functions for pending activation
+    void ResetPendingActivation();
+    REAL GetPendingActivation();
 
-	// drawing helper functions
-	void DrawTitleBand(CDC *pDC, CRect& rectInner);
-	void DrawTitle(CDC *pDC, CRect& rectInner);
-	void DrawText(CDC *pDC, CRect& rectInner);
-	void DrawImage(CDC *pDC, CRect& rectInner);
+    // drawing helper functions
+    void DrawTitleBand(CDC *pDC, CRect& rectInner);
+    void DrawTitle(CDC *pDC, CRect& rectInner);
+    void DrawText(CDC *pDC, CRect& rectInner);
+    void DrawImage(CDC *pDC, CRect& rectInner);
 
 private:
-	// stores the node
-	CNode* m_pNode;
+    // stores the node
+    CNode* m_pNode;
 
-	// stores the center and spring-connected center of the node view
-	CVectorD<3> m_vSpringCenter;
+    // stores the center and spring-connected center of the node view
+    CVectorD<3> m_vSpringCenter;
 
-	// stores the thresholded activation and spring-connected activation
-	REAL m_springActivation;
+    // stores the thresholded activation and spring-connected activation
+    REAL m_springActivation;
 
-	// stores the pending activation
-	REAL m_pendingActivation;
+    // stores the pending activation
+    REAL m_pendingActivation;
 
-	// stores the node view's region (shape)
-	CRect m_rectOuter;
-	CRect m_rectInner;
+    // stores the node view's region (shape)
+    CRect m_rectOuter;
+    CRect m_rectInner;
 
-	CRgn m_shape;
+    CRgn m_shape;
 
-	// flags to indicate dragging state
-	BOOL m_bDragging;
-	BOOL m_bDragged;
+    // flags to indicate dragging state
+    BOOL m_bDragging;
+    BOOL m_bDragged;
 
-	// stores the mouse-down point
-	CPoint m_ptMouseDown;
+    // stores the mouse-down point
+    CPoint m_ptMouseDown;
 
-	// stores the windows parent CSpaceView window
-	CSpaceView *m_pParent;
+    // stores the windows parent CSpaceView window
+    CSpaceView *m_pParent;
 
-	// flag to indicate image is to be drawn in background
-	BOOL m_bBackgroundImage;
-};
+    // flag to indicate image is to be drawn in background
+    BOOL m_bBackgroundImage;
+
+};	// class CNodeView
+
 
 /////////////////////////////////////////////////////////////////////////////
 
