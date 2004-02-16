d4df2066ab29f06aa536c0fce96faa71a4a6e7a9
default <>
2004-02-16 15:52:07 +0000

    Added test gradient function

--- OptimizeN/ObjectiveFunction.cpp
+++ OptimizeN/ObjectiveFunction.cpp
@@ -22,3 +22,33 @@ BOOL CObjectiveFunction::HasGradientInfo() const
 {
 	return m_bHasGradientInfo;
 }
+
+
+REAL CObjectiveFunction::TestGradient(const CVectorN<>& vAtParam, REAL epsilon) const
+{
+	CVectorN<> vParam = vAtParam;
+	CVectorN<> vEvalGrad;
+
+	REAL fp = (*this)(vParam, &vEvalGrad);
+
+	// test the gradient
+	CVectorN<> vDiffGrad;
+	vDiffGrad.SetDim(vParam.GetDim());
+
+	for (int nAt = 0; nAt < vParam.GetDim(); nAt++)
+	{
+		vParam[nAt] += epsilon;
+
+		REAL fp_del = (*this)(vParam);
+
+		vDiffGrad[nAt] = (fp_del - fp);
+		vDiffGrad[nAt] /= epsilon;
+
+		vParam[nAt] -= epsilon;
+	}
+
+	TRACE_VECTOR("Eval Grad = ", vEvalGrad);
+	TRACE_VECTOR("Diff Grad = ", vDiffGrad);
+
+	return (vEvalGrad - vDiffGrad).GetLength();
+}
