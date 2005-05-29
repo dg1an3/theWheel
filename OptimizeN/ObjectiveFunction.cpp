fb6863bc8497b439b01c56d94d9dee86be12208e
HP_Administrator <>
2005-05-29 22:34:41 +0000

    changed TestGradient to Gradient

--- OptimizeN/ObjectiveFunction.cpp
+++ OptimizeN/ObjectiveFunction.cpp
@@ -37,65 +37,89 @@ BOOL CObjectiveFunction::HasGradientInfo() const
 
 
 ///////////////////////////////////////////////////////////////////////////////
-// CObjectiveFunction::TestGradient
+// CObjectiveFunction::Transform
 // 
-// <description>
+// over-ride for function to transform parameters
 ///////////////////////////////////////////////////////////////////////////////
-REAL CObjectiveFunction::TestGradient(const CVectorN<>& vAtParam, double epsilon) const
+void CObjectiveFunction::Transform(CVectorN<> *pvInOut) const
+{
+	// default is identity transform
+	ASSERT(false);
+
+}	// CObjectiveFunction::Transform
+
+///////////////////////////////////////////////////////////////////////////////
+// CObjectiveFunction::dTransform
+// 
+// over-ride for derivative transform parameters
+///////////////////////////////////////////////////////////////////////////////
+void CObjectiveFunction::dTransform(CVectorN<> *pvInOut) const
+{
+	// set to all 1.0 for identity transform
+	ITERATE_VECTOR((*pvInOut), nAt, (*pvInOut)[nAt] = 1.0);
+
+}	// CObjectiveFunction::dTransform
+
+///////////////////////////////////////////////////////////////////////////////
+// CObjectiveFunction::InvTransform
+// 
+// inverse of parameter transform
+///////////////////////////////////////////////////////////////////////////////
+void CObjectiveFunction::InvTransform(CVectorN<> *pvInOut) const
+{
+	// default is identity transform
+	ASSERT(false);
+
+}	// CObjectiveFunction::InvTransform
+
+
+///////////////////////////////////////////////////////////////////////////////
+// CObjectiveFunction::Gradient
+// 
+// approximates gradient using difference method
+///////////////////////////////////////////////////////////////////////////////
+void CObjectiveFunction::Gradient(const CVectorN<>& vIn, REAL epsilon, 
+				CVectorN<>& vGrad_out) const
 {
 	REAL res = 0.0;
-	BEGIN_LOG_SECTION(CObjectiveFunction::TestGradient());
+	BEGIN_LOG_SECTION(CObjectiveFunction::Gradient());
 
 	// get epsilon
 	REAL elem_max = 0.0;
-	for (int nAt = 0; nAt < vAtParam.GetDim(); nAt++)
+	for (int nAt = 0; nAt < vIn.GetDim(); nAt++)
 	{
-		elem_max = __max(elem_max, fabs(vAtParam[nAt]));
+		elem_max = __max(elem_max, fabs(vIn[nAt]));
 	}
 	epsilon = elem_max * epsilon;
 
-	CVectorN<> vEvalGrad;
-	vEvalGrad.SetDim(vAtParam.GetDim());
-	Gradient(vAtParam, vEvalGrad);
-	LOG_EXPR_EXT(vEvalGrad);
-
 	// numerically evaluate the gradiant
-	CVectorN<> vParam = vAtParam;
-	const double fp = (*this)(vParam);
+	CVectorN<> vParam = vIn;
+	const REAL fp = (*this)(vParam);
 
-	CVectorN<> vDiffGrad;
-	vDiffGrad.SetDim(vParam.GetDim());
+	vGrad_out.SetDim(vParam.GetDim());
 	for (nAt = 0; nAt < vParam.GetDim(); nAt++)
 	{
-		vParam[nAt] += (REAL) epsilon;
+		vParam[nAt] += epsilon;
 
-		const double fp_del = (*this)(vParam);
+		const REAL fp_del = (*this)(vParam);
+		vGrad_out[nAt] = (fp_del - fp) / epsilon;
 
-		vDiffGrad[nAt] = (REAL)((fp_del - fp) / epsilon);
-		// vDiffGrad[nAt] /= epsilon;
-
-		vParam[nAt] -= (REAL) epsilon;
+		vParam[nAt] -= epsilon;
 	}
-	LOG_EXPR_EXT(vDiffGrad);
+	LOG_EXPR_EXT(vGrad_out);
 
-	res = (vEvalGrad - vDiffGrad).GetLength();
-	TRACE_VECTOR("vEvalGrad", vEvalGrad);
-	TRACE_VECTOR("vDiffGrad", vDiffGrad);
-	// TRACE_VECTOR("Gradient diff", vEvalGrad - vDiffGrad);
-	LOG_EXPR(res);
-	END_LOG_SECTION();	// CObjectiveFunction::TestGradient
+	END_LOG_SECTION();	// CObjectiveFunction::Gradient
 
-	return res;
-
-}	// CObjectiveFunction::TestGradient
+}	// CObjectiveFunction::Gradient
 
 
 ///////////////////////////////////////////////////////////////////////////////
 // CObjectiveFunction::Hessian
 // 
-// <description>
+// approximates hessian using difference method
 ///////////////////////////////////////////////////////////////////////////////
-void CObjectiveFunction::Hessian(const CVectorN<>& vAtParam, CMatrixNxM<>& mHess_out) const
+void CObjectiveFunction::Hessian(const CVectorN<>& vAtParam, REAL epsilon,
+								CMatrixNxM<>& mHess_out) const
 {
 	BEGIN_LOG_SECTION(CObjectiveFunction::Hessian());
 
@@ -109,7 +133,7 @@ void CObjectiveFunction::Hessian(const CVectorN<>& vAtParam, CMatrixNxM<>& mHess
 		BEGIN_LOG_SECTION_(FMT("Col %i", nAtCol));
 
 		vParam = vAtParam;
-		vParam[nAtCol] += DEFAULT_EPSILON;
+		vParam[nAtCol] += epsilon;
 		const REAL fp_del_col = (*this)(vParam);
 
 		for (int nAtRow = 0; nAtRow < mHess_out.GetRows(); nAtRow++)
@@ -117,14 +141,14 @@ void CObjectiveFunction::Hessian(const CVectorN<>& vAtParam, CMatrixNxM<>& mHess
 			BEGIN_LOG_SECTION_(FMT("Row %i", nAtRow));
 
 			vParam = vAtParam;
-			vParam[nAtRow] += DEFAULT_EPSILON;
+			vParam[nAtRow] += epsilon;
 			const REAL fp_del_row = (*this)(vParam);
-			vParam[nAtCol] += DEFAULT_EPSILON;
+			vParam[nAtCol] += epsilon;
 			const REAL fp_del_row_del_col = (*this)(vParam);
 
 			mHess_out[nAtCol][nAtRow] = fp_del_row_del_col - fp_del_row;
 			mHess_out[nAtCol][nAtRow] -= fp_del_col - fp;
-			mHess_out[nAtCol][nAtRow] /= DEFAULT_EPSILON * DEFAULT_EPSILON;
+			mHess_out[nAtCol][nAtRow] /= epsilon * epsilon;
 			LOG_EXPR(mHess_out[nAtCol][nAtRow]);
 
 			END_LOG_SECTION();	// FormatString("Row %i", nAtRow);
@@ -136,57 +160,3 @@ void CObjectiveFunction::Hessian(const CVectorN<>& vAtParam, CMatrixNxM<>& mHess
 	END_LOG_SECTION();	// CObjectiveFunction::Hessian
 
 }	// CObjectiveFunction::Hessian
-
-
-///////////////////////////////////////////////////////////////////////////////
-// CObjectiveFunction::TestHessian
-// 
-// <description>
-///////////////////////////////////////////////////////////////////////////////
-void CObjectiveFunction::TestHessian(const CVectorN<>& vAtParam, double epsilon) const
-{
-	BEGIN_LOG_SECTION(CObjectiveFunction::TestHessian());
-
-	CMatrixNxM<> mEvalHess;
-	Hessian(vAtParam, mEvalHess);
-	LOG_EXPR_EXT(mEvalHess);
-
-	const REAL fp = (*this)(vAtParam);
-
-	// numerically evaluate the hessian
-	CVectorN<> vParam = vAtParam;
-
-	CMatrixNxM<> vDiffHess;
-	vDiffHess.Reshape(vParam.GetDim(), vParam.GetDim());
-	for (int nAtCol = 0; nAtCol < vParam.GetDim(); nAtCol++)
-	{
-		BEGIN_LOG_SECTION_(FMT("Col %i", nAtCol));
-
-		vParam = vAtParam;
-		vParam[nAtCol] += (REAL) epsilon;
-		const REAL fp_del_col = (*this)(vParam);
-
-		for (int nAtRow = 0; nAtRow < vDiffHess.GetRows(); nAtRow++)
-		{
-			BEGIN_LOG_SECTION_(FMT("Row %i", nAtRow));
-
-			vParam = vAtParam;
-			vParam[nAtRow] += (REAL) epsilon;
-			const REAL fp_del_row = (*this)(vParam);
-			vParam[nAtCol] += (REAL) epsilon;
-			const REAL fp_del_row_del_col = (*this)(vParam);
-
-			vDiffHess[nAtCol][nAtRow] = fp_del_row_del_col - fp_del_row;
-			vDiffHess[nAtCol][nAtRow] -= fp_del_col - fp;
-			vDiffHess[nAtCol][nAtRow] /= (REAL) (epsilon * epsilon);
-			LOG_EXPR(vDiffHess[nAtCol][nAtRow]);
-
-			END_LOG_SECTION();	// FormatString("Row %i", nAtRow);
-		}
-		END_LOG_SECTION();	// FormatString("Row %i", nAtCol);
-	}
-	LOG_EXPR_EXT(vDiffHess);
-
-	END_LOG_SECTION();	// CObjectiveFunction::TestHessian
-
-}	// CObjectiveFunction::TestHessian
