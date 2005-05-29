e87e44530a5fbeaaf7861627ae187ec09474da02
HP_Administrator <>
2005-05-29 22:20:49 +0000

    Fixed AfxGetApp problem

--- MTL/MathUtil.h
+++ MTL/MathUtil.h
@@ -234,6 +234,8 @@ inline TYPE InvSigmoid(TYPE y, TYPE scale /* = 1.0 */)
 { for (int index = 0; index < coll.GetDim(); index++) { statement; } }
 
 
+// only use if we have included necessary AFX calls
+#ifdef _AFXWIN_INLINE
 //////////////////////////////////////////////////////////////////////
 // GetProfileReal
 //
@@ -257,6 +259,7 @@ inline REAL GetProfileReal(const char *pszSection, const char *pszName, double d
 	return value;
 
 }	// GetProfileReal
+#endif
 
 
 //////////////////////////////////////////////////////////////////////
