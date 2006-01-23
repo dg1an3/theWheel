d195484a5c10c809a86df7efdef8a2b60c0bdae1
HP_Administrator <>
2006-01-23 02:15:19 +0000

    removed deprecated call

--- theWheel/theWheel.cpp
+++ theWheel/theWheel.cpp
@@ -64,12 +64,6 @@ BOOL theWheelApp::InitInstance()
 	//  of your final executable, you should remove from the following
 	//  the specific initialization routines you do not need.
 
-#ifdef _AFXDLL
-	Enable3dControls();			// Call this when using MFC in a shared DLL
-#else
-	Enable3dControlsStatic();	// Call this when linking to MFC statically
-#endif
-
 	// initialize OLE
 	if (!::AfxOleInit())
 	{
