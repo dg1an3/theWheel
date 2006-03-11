8be2fcf33eb5597fefc52754b6b02a98f06d3367
HP_Administrator <>
2006-03-11 03:52:39 +0000

    added resources for perturb / scale

--- theWheel/resource.h
+++ theWheel/resource.h
@@ -1,5 +1,5 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Developer Studio generated include file.
+// Microsoft Visual C++ generated include file.
 // Used by theWheel.rc
 //
 #define IDD_ABOUTBOX                    100
@@ -9,9 +9,16 @@
 #define IDR_SPACETYPE                   129
 #define IDD_DIALOG1                     130
 #define IDD_PERTURBDIALOG               131
+#define IDD_SCALEDIALOG                 132
 #define IDC_EDIT_AMOUNT                 1000
+#define IDC_EDIT1                       1001
+#define IDC_EDIT2                       1002
 #define ID_RUNTIME                      32773
 #define ID_PERTURB                      32774
+#define ID_SPACE_PERTURBLINKS           32775
+#define ID_SPACE_SCALELINKS             32776
+#define ID_SPACE_DESIGNMODE             32777
+#define ID_Menu                         32778
 #define IDS_HELPFILE_REL_PATH           61446
 
 // Next default values for new objects
@@ -19,9 +26,9 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_3D_CONTROLS                     1
-#define _APS_NEXT_RESOURCE_VALUE        132
-#define _APS_NEXT_COMMAND_VALUE         32775
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_RESOURCE_VALUE        133
+#define _APS_NEXT_COMMAND_VALUE         32779
+#define _APS_NEXT_CONTROL_VALUE         1003
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
